70c79e0b90962b73643d85c7bf12c6ba617cb7d1
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2022-11-28 17:00:03 -0800

    added color testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    color:{
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      'goldenrod': '#925B03',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
   },
   variants: {
     extend: {},
