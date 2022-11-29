2d3a83ca4d87fb2898da3a1641e671e457b17295
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2022-11-29 09:51:19 -0800

    commt

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center text-sm">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Matt.
@@ -106,7 +106,7 @@ export default function About() {
           />
 
           <div className="flex justify-center space-x-3 mt-3">
-            <div className="">
+            <div className="text-sm">
               <img
                 className="object-cover object-center rounded"
                 alt="placeholder image1"
@@ -115,7 +115,7 @@ export default function About() {
               <p> caption something</p>
 
             </div>
-            <div className="">
+            <div className="text-sm">
               <img
                 className="object-cover object-center rounded"
                 alt="placeholder image2"
