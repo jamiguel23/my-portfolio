ece35a7f418a8e2ffc3d16775611e448f6902074
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-06-08 21:05:44 -0700

    fixed rob and anjalli pics, added kiki rec, and added text with link for project 2

--- src/data.js
+++ src/data.js
@@ -4,17 +4,17 @@ export const projects = [
     title: "Horned Beast",
     subtitle: "React.js",
     description:
-      "Ever wanted to vote on your favorite horned breast? Now you can with this exrensive list of your favorite beast real and imaginary. Add likes to your favorite and fliter but number of horns. Which is your favorite?",
+      "Explore an extensive list of real and imaginary beasts, vote on your favorite horned breasts, and filter by the number of horns. Discover your favorite!",
     image: "./horn-beast.gif",
     link: "https://hornedbeast-jamiguel23.netlify.app/",
   },
   {
-    title: "React Tracks",
-    subtitle: "Placeholder",
+    title: "Memory Game",
+    subtitle: "HTML, CSS, Vanilla JS",
     description:
-      "Placeholder sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Dynamically mix and match HTML and CSS terms in this engaging memory game, enhancing web development knowledge and testing memory skills.",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "hhttps://theta-btm260.github.io/memory-game/",
   },
   {
     title: "DevChat",
@@ -38,16 +38,23 @@ export const testimonials = [
   {
     quote:
       "Matt has an extraordinary ability to communicate effectively and listens to the needs of others, which serves him well in understanding and relaying information expertly. Matt is flexible in adapting to changes in processes or plans, rising to the challenge of problem solving on the fly. Matt forms strong working relationships by being earnest and bringing a positive outlook each day. I was fortunate to have Matt on my team over an intense period of growth, where he rose to the challenge by taking on progressive levels of responsibility. ",
-    image: 'https://media.licdn.com/dms/image/D5603AQHh690svUPYFA/profile-displayphoto-shrink_800_800/0/1674447321426?e=1680134400&v=beta&t=vR-z2x7lt22TNHeQLGcVLkqrScg6_nwA9jy-BDZNj5M',
+    image: './rob.jpeg',
     name: "Robert Schmuck",
     company: "Amazon",
   },
   {
     quote:
       "Matt delivers strong working results, adapts quickly to new processes, and exhibits positive influence each day in a dynamic, diverse, and highly-evolving work environment. Matt excels at building and growing strong working relationships with team members, leadership, as well as cross functional teams. He also provides first-class support in each and every interaction with consumers, truly exemplifying his ability to listen to the needs of the end user. Matt will be an asset on any team, and especially as a Software Developer because he has a deep understanding of human connection. I'm lucky to have had him on my team!",
-    image: "https://media.licdn.com/dms/image/D5603AQEJDlMHvvN9kA/profile-displayphoto-shrink_800_800/0/1668727320090?e=1680134400&v=beta&t=-79pGAF0mMxaw04pawtXm3UQ3_48CW6HT7boo9sCFfg",
+    image: "./anjalli.jpeg",
     name: "Anjali Billington",
     company: "Zillow Group",
+  },
+  {
+    quote:
+      "I've had the pleasure of working with Matt at Year Up and have witnessed him build community, skill, and expertise in the software development world. Matt is someone who regularly demonstrates a willingness to learn - moving through challenges and even discomfort with a growth mindset. Matt displays leadership, grace, and authenticity in all his actions -- I'm confident that his eagerness to learn, grow, and build relationships will be an asset to any team. I wholeheartedly recommend Matt as a professional, and am excited to see where his journey takes him.",
+    image: "./kiki.jpeg",
+    name: "Kiki Chocklett",
+    company: "Year Up",
   },
 ];
 
